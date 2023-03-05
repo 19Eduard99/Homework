26bedd3a8d33161d8e10cd95637faaf64fab7369
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-03-05 17:47:42 +0200

    hw_5

--- script/index.js
+++ script/index.js
@@ -1,1 +1,23 @@
 'use strict';
+
+let objectUser = {}
+objectUser.name = 'John';
+objectUser.surname = 'Smith';
+objectUser.name = 'Pete';
+delete objectUser.name
+
+// объект объявленный с помощью const можно изменить потомучто это ссылочный тип данных
+const user = {
+    name: 'John'
+};
+user.name = 'Pete';
+console.log(user);
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+const sum = salaries.John + salaries.Ann + salaries.Pete
+console.log(sum);
