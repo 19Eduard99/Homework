226226a0cb85a9177cfd239de615e64766f8f51c
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-03-05 17:22:34 +0200

    hw_2

--- script/index.js
+++ script/index.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const a = parseFloat(prompt("a = ",));
+const b = parseFloat(prompt("b = ",));
+function sum(a, b) {
+    return a + b;
+}
+alert("Result: " + a +' + '+ b + ' = ' + sum(a,b));
+
+function minus(a, b){
+    return a - b;
+}
+alert("Result: " + a +' - '+ b + ' = ' +  minus(a,b));
+
+function multiply(a, b){
+    return a * b;
+}
+alert("Result: " + a +' * '+ b + ' = ' + multiply(a,b));
+
+function divide(a, b){
+    return a / b;
+}
+alert("Result: " + a +' / '+ b + ' = ' + divide(a,b));
