31e3602b524d38919193472a9a52abe1441e7979
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-03-05 17:38:36 +0200

    hw_4

--- script/index.js
+++ script/index.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const operator = prompt("Enter ( +, -, *, / ):");
+
+
+const number1 = parseFloat(prompt("Enter number 1: "));
+const number2 = parseFloat(prompt("Enter number 2: "));
+let result;
+
+if (operator === '+') {
+
+    result = number1 + number2;
+    alert("Result: " + number1 + operator + number2 + ' = ' +result);
+}
+
+else if (operator === '-') {
+    result = number1 - number2;
+    alert("Result: " + number1 + operator + number2 + ' = ' +result);
+}
+else if (operator === '*') {
+    result = number1 * number2;
+    alert("Result: " + number1 + operator + number2 + ' = ' +result);
+}
+else if (operator === '/') {
+    result = number1 * number2;
+    alert("Result: " + number1 + operator + number2 + ' = ' +result);
+}
+else {
+    alert("error");
+}
