45f357246e0a0a3f4a7c986304367a76b267faf7
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-03-13 15:03:31 +0200

    hw_7

--- script/index.js
+++ script/index.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const a = +prompt("Enter first number ");
+const b = +prompt("Enter second number ");
+
+function fun1(a, b) {
+     return a === b;
+}
+console.log(fun1(a,b));
+
+function fun2(a, b) {
+    return (a + b) > 10;
+}
+console.log(fun2(a,b));
+
+const c = +prompt("Enter  number ");
+function fun3(c) {
+    return c < 0;
+}
+console.log(fun3(c));
