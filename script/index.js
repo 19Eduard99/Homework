623973d1ae9fd0ef7fd5f84b6adde1b97be6045b
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-03-14 21:25:35 +0200

    hw_10_1

--- script/index.js
+++ script/index.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const year = +prompt('В какому году ты родился?');
+
+const city = prompt('В каком городе ты живешь?');
+
+const sport = prompt('Какой твой любимый вид спорта?');
+
+alert(year + city + sport);
