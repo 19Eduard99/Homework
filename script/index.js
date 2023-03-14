c3033566d7fcfd3d80fc1066c4259b277a46c854
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-03-14 15:43:47 +0200

    hw_8

--- script/index.js
+++ script/index.js
@@ -1,1 +1,29 @@
 'use strict';
+
+// Задание 1
+
+const  a = +prompt('Введите год моего рождения');
+
+if(a < 1999  || a > 1999 || isNaN(a)) {
+    console.log('Неверно');
+} else {
+    console.log("Верно");
+}
+
+//Задание 2
+
+let numOrStr1 = prompt('input number or string');
+
+switch (true){
+    case (numOrStr1 === null):
+        console.log('вы отменили');
+        break;
+    case  (numOrStr1.trim() === ''):
+        console.log('Empty String');
+        break;
+    case (isNaN(+numOrStr1)):
+        console.log(' number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
+}
