4100b5fadbe22338ec796b7ba9c28e0194652f2e
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-03-15 16:48:42 +0200

    hw_12

--- script/index.js
+++ script/index.js
@@ -5,7 +5,5 @@ const y = 9;
 const r = 10;
 
 const lengthHypotenuse  = Math.sqrt(x*x + y*y);
-
 const result = (lengthHypotenuse <= r) ? "Точка внутри окружности" : "Точка не внутри окружности";
-
 console.log(result);
