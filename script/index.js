0f77eeb6e027c729cf32fe6f1cf6b3f83486d63d
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-03-20 12:59:57 +0200

    hw_13

--- script/index.js
+++ script/index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const age = prompt('Введите свой возраст: ');
+let word;
+
+if (age !== null && !isNaN(age)) {
+    if (age % 10 === 1 && age % 100 !== 11) {
+        word = 'год';
+    } else if (age % 10 >= 2 && age % 10 <= 4 && (age % 100 < 10 || age % 100 >= 20)) {
+        word = 'года';
+    } else {
+        word = 'лет';
+    }
+    alert('Ваш возраст: ' + age + ' ' + word);
+} else {
+    alert('Не правильный возраст');
+}
