ea475d2ca824446abfa780294649731241d09d8b
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-03-21 19:11:40 +0200

    hw_16

--- script/index.js
+++ script/index.js
@@ -1,1 +1,15 @@
 'use strict';
+let a = +prompt('Введите число ');
+let b = +prompt('Введите степень ');
+
+function f1(a,b =1 ) {
+   if(isNaN(a) || isNaN(b)){
+       return alert('error');
+   }
+   else {
+       let step = a ** b;
+       return alert(step);
+   }
+}
+
+let result = f1(a,b);
