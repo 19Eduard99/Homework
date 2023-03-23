74f89d3976e0d322ac332e6eded3fe15744cf58e
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-03-23 17:04:18 +0200

    hw_18

--- script/index.js
+++ script/index.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const arr = [1, 2, 3, -1, -2, -3];
+
+function myArray(array) {
+    const exampleArr = [];
+    if(!array.length) return 'Пустой массив';
+
+    for (let i = 0; i < array.length; i++){
+        if(array[i] > 0){
+            exampleArr.push(array[i]);
+        }
+    }
+    if(!exampleArr.length) return null;
+
+    return exampleArr;
+}
+
+console.log(myArray(arr));
