112170be316db8f039a841ae6b73b6da9b44cea0
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-03-24 14:25:35 +0200

    hw_20_2

--- script/index.js
+++ script/index.js
@@ -1,6 +1,13 @@
 'use strict';
 function fib(n = 1) {
-    return (n - 1) + (n - 2);
+    const result = [0, 1];
+    for(let i = 2; i <= n; i++){
+        const prevN1 = result[i - 1];
+        const prevN2 = result[i - 2];
+        result.push(prevN1 + prevN2);
+    }
+
+    return result[n];
 }
 
 console.log(fib(10));
