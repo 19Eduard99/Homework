22bfc7c5c55a577e235864c6528ec26c15c0be34
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-03-24 12:26:18 +0200

    hw_16_2

--- script/index.js
+++ script/index.js
@@ -3,14 +3,9 @@ const a = prompt('Введите число ');
 const b = prompt('Введите степень ');
 
 function f1(a,b =1 ) {
-    if (a !== null || b === null) return alert('error')
-   if(isNaN(a) || isNaN(b)){
-       return alert('error');
-   }
-   else {
-       const step = a ** b;
-       return alert(step);
-   }
+   if(isNaN(a) || isNaN(b)) return  alert('error');
+   return Math.pow(a, b);
 }
 
-const result = f1(a,b);
+const result = f1(a, b);
+alert(result);
