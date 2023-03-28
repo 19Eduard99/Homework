564f576c4b44ceca39b08e9bd367fd2b2a1c2b90
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-03-28 22:13:19 +0300

    hw_21_2

--- script/index.js
+++ script/index.js
@@ -1,4 +1,16 @@
 'use strict';
 
-const num = prompt('Enter number');
+const num = +prompt('Enter number');
+let bol = true
+if (num < 2) {
+    bol = false
+    console.log(bol)
+}   else {
+        for (let  i = 2; i < num; i++){
+            if (num % i === 0){
+                bol = false
+            }
+        }
+    console.log(bol)
+}
 
