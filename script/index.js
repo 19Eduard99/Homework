7dbb88f65b6b3565eada0835d84b8521e5ee3b55
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-03-29 00:47:02 +0300

    hw_21

--- script/index.js
+++ script/index.js
@@ -4,13 +4,13 @@ const num = +prompt('Enter number');
 let bol = true
 if (num < 2) {
     bol = false
-    console.log(bol)
 }   else {
         for (let  i = 2; i < num; i++){
             if (num % i === 0){
-                bol = false
+                bol = false;
+                break
             }
         }
-    console.log(bol)
 }
+console.log(bol)
 
