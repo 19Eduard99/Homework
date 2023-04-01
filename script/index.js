5bf8be5a42a5352eeb26f1357d00e8e63d7047ed
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-04-01 15:06:19 +0300

    hw_21_23

--- script/index.js
+++ script/index.js
@@ -1,15 +1,15 @@
 'use strict';
 const num  = prompt('Введите число ');
-const degree  = prompt('Введите степень ');
+const degree  = +prompt('Введите степень ');
 
 function  operation(num ,degree  = 1 ) {
-   if(isNaN(num) || isNaN(degree) || num === null || degree  === null || num.trim() === "" || degree.trim() === ""){
-      return  alert('error');
+   if(isNaN(num) || isNaN(degree) || num === null || degree  === null || num.trim() === ""){
+      return 'error';
+   } else {
+      return num ** degree;
    }
-      return Math.pow(num , degree );
 }
 
 const result = operation(num , degree);
-if(result !== undefined){
-   alert(result);
-}
+alert(result);
+
