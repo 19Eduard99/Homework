2d7fafd018e75ccb300f1e5e851c00e7ff04efa6
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-04-01 15:42:05 +0300

    hw_22

--- script/index.js
+++ script/index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const number = +prompt("Введите число:");
+
+if (isNaN(number) || number <= 1) {
+    console.log(NaN);
+} else {
+    let i = 2;
+    while (number % i !== 0) {
+        i++;
+    }
+    if (number === i) {
+        alert('Число ' + number + ' простое');
+    } else {
+        alert('Наименьший делитель числа ' + number + ' = ' + i);
+    }
+}
