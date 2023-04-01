670d74912f65919e5825cd6770d81196af91c5a4
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-04-01 19:15:59 +0300

    hw_19

--- script/index.js
+++ script/index.js
@@ -1,12 +1,9 @@
 'use strict';
 
-function potato(lBorscht, priceKg) {
-    const potatoInGrams  = 0.075;
-    const OneLiterOfPotatoes = 4;
+function potato(lBorscht, priceKg, potatoInGrams, OneLiterOfPotatoes) {
     const weight = lBorscht * OneLiterOfPotatoes;
-
     const tWeight = weight * potatoInGrams;
     return Math.ceil(tWeight * priceKg)
 }
 
-console.log(potato(48, 13))
+console.log(potato(48, 13 , 0.075 ,4))
