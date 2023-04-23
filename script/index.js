f99e7c89d08176faf5e643a322aebe2807f6b3fb
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-04-23 23:11:19 +0300

    hw_26

--- script/index.js
+++ script/index.js
@@ -1,1 +1,80 @@
 'use strict';
+
+//1
+const array = [2, 5, 7, 9, 9];
+
+const myIndexOf = (arr, elm) => {
+    for (let i = 0; i < arr.length; i++){
+        if(arr[i] === elm){
+            return i;
+        }
+    }
+    return -1;
+}
+console.log(myIndexOf(array,9));
+
+//2
+const myLastIndexOf = (arr, elm) => {
+    for (let i = arr.length - 1; i >= 0; i--){
+        if(arr[i] === elm){
+            return i;
+        }
+    }
+    return -1;
+}
+console.log(myLastIndexOf(array,9));
+
+//3
+const myFind = (arr) => {
+        for (let i = 0; i < arr.length; i++){
+            if(arr[i] === 7) {
+                return arr[i];
+            }
+        }
+    return undefined;
+}
+console.log(myFind(array));
+
+//4
+const myFindIndex = (arr) => {
+    for (let i = 0; i < arr.length; i++){
+        if(arr[i] === 7) {
+            return i;
+        }
+    }
+    return undefined;
+}
+console.log(myFindIndex(array));
+
+//5
+const myIncludes = (arr, num) => {
+    for (let i = 0; i < arr.length; i++){
+        if(arr[i] === num) {
+            return true;
+        }
+    }
+    return false;
+}
+console.log(myIncludes(array, 7));
+
+//6
+const myEvery = (arr) => {
+    for (let i = 0; i < arr.length; i++){
+        if(arr[i] < 1){
+            return false
+        }
+    }
+    return true
+}
+console.log(myEvery(array));
+
+//7
+const mySome = (arr) => {
+    for (let i = 0; i < arr.length; i++){
+        if(arr[i] >= 1){
+            return true
+        }
+    }
+    return false
+}
+console.log(mySome(array));
