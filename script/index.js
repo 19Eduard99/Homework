fbb1faff01aabf9c7a6bd2097316e73e6fa3744a
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-05-02 22:50:46 +0300

    hw_31

--- script/index.js
+++ script/index.js
@@ -1,1 +1,14 @@
 'use strict';
+
+function MyPalindrome(num, steps = 0) {
+    const number =  parseInt(num.toString().split('').reverse().join(''));
+    if (num === number) {
+        return { result: num, steps: steps };
+    } else {
+        const sum = num + number;
+        return MyPalindrome(sum, steps + 1);
+    }
+
+}
+const palindrome = MyPalindrome(96);
+console.log(palindrome)
