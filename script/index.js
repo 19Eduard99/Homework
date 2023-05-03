11cc2b507137d360569e7edcd403ba28cfc42004
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-05-03 23:55:38 +0300

    hw_32

--- script/index.js
+++ script/index.js
@@ -1,1 +1,18 @@
 'use strict';
+
+void function () {
+    const array = [1, 2, [3, 4], [[5 ,6], 7]];
+    const flat = (...args) => {
+        if (args.length !== 1) throw new Error('Function accepts only 1 argument, too much arguments provided');
+        const result = [];
+        args[0].forEach((item) => {
+            if (Array.isArray(item)) {
+                return result.push(...flat(item));
+            } else {
+                return result.push(item);
+            }
+        })
+        return result;
+    }
+    console.log(flat(array));
+}();
