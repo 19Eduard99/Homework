47eef8017b7a234981fbd096c7f094e7504136f7
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-05-06 00:33:23 +0300

    hw_36

--- script/index.js
+++ script/index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+let calculator = {
+    read: () => {
+        this.a = +prompt('Введи первое число ');
+        this.b = +prompt('Введи второе число ');
+        if (isNaN(this.a) || isNaN(this.b)) {
+            alert('Введи числа')
+            calculator.read()
+        }
+    },
+    sum: () => {return this.a + this.b},
+    mul: () => {return this.a * this.b}
+};
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
