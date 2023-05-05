a14f050c0103093c178f078e01514422debf167e
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-05-05 21:59:23 +0300

    hw_34

--- script/index.js
+++ script/index.js
@@ -1,1 +1,27 @@
 'use strict';
+
+void  function () {
+  const generateRandomNum = () => {
+      const  arrNum = [];
+
+      const numGenerator = () => {
+          if (arrNum.length === 100) return NaN;
+          const randomNum = Math.floor(Math.random() * 100) + 1;
+
+          if(arrNum.includes(randomNum)){
+              return numGenerator()
+          }
+          arrNum.push(randomNum);
+          return randomNum;
+      }
+      return numGenerator;
+  }
+
+  const numGenerator = generateRandomNum();
+    let res = true;
+    while (res) {
+        const genNum = numGenerator();
+        if(isNaN(genNum)) res = false;
+        console.log(genNum)
+    }
+}()
