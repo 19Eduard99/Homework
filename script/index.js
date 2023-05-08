787ec1707340cd7d3d15071d1f38f89bc440c4a1
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-05-09 00:17:17 +0300

    hw_39

--- script/index.js
+++ script/index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const findLi = () => {
+    const arr = [];
+    const arrText = [];
+    for (const el of document.querySelectorAll('li')) {
+        arr.push(el);
+        arrText.push(el.innerText);
+    }
+    return [arr, arrText]
+}
+const [arr, arrText] = findLi()
+for (const el of arr) {
+    console.log(el);
+}
+console.log(arrText);
+console.log(arr.length);
